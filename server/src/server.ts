03f38fecf6c66e2ea2c8263a7220d482c93e3f84
Parsa Mh <157930237+parsamh8@users.noreply.github.com>
2024-12-06 12:57:51 -0500

    fix server path

--- server/src/server.ts
+++ server/src/server.ts
@@ -28,13 +28,13 @@ const startApolloServer = async () => {
     }
   ));
 
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+  // if (process.env.NODE_ENV === 'production') {
+  //   app.use(express.static(path.join(__dirname, '../client/dist')));
 
-    app.get('*', (_req: Request, res: Response) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-    });
-  }
+  //   app.get('*', (_req: Request, res: Response) => {
+  //     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  //   });
+  // }
 
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);

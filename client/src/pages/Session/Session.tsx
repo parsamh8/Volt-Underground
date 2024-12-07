190d0afbcb3733ab91b4baf3eead8656ca9fe8f1
Parsa Mh <157930237+parsamh8@users.noreply.github.com>
2024-12-07 17:33:50 -0500

    complete session init UI

--- client/src/pages/Session/Session.tsx
+++ client/src/pages/Session/Session.tsx
@@ -1,10 +1,21 @@
-const Session = ({ posterUrl, title, description, venue, date, time, ticketLink }) => {
+import './index.css'
+
+interface Class {
+  posterUrl: string,
+  title: string,
+  description: string,
+  venue: string,
+  date: string,
+  time: string,
+  ticketLink: string
+}
+
+const Session = ({ posterUrl, title, description, venue, date, time, ticketLink }: Class) => {
   return (
-    <div className="event-session">
+    <div className="event-session Ticket">
       <div className="event-details">
         {/* Event Poster */}
-        <div className="event-poster">
-          <img src={posterUrl} alt={`${title} Poster`} />
+        <div className="event-border">
         </div>
 
         {/* Event Description */}
@@ -16,14 +27,21 @@ const Session = ({ posterUrl, title, description, venue, date, time, ticketLink 
 
       {/* Separator Line */}
       <div className="separator"></div>
+        <div className='session-img'>
+
+        </div>
 
       {/* Venue Information */}
       <div className="venue-info">
-        <h2>Venue Information</h2>
+        <h2>Session Information</h2>
+        <p><strong>Description:</strong>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti consequatur temporibus ducimus eligendi voluptatibus porro. Doloribus repellat architecto incidunt ullam eaque enim velit laudantium, tempora earum! Dolor eaque cum deleniti?Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, soluta ratione quisquam officia amet voluptate exercitationem velit impedit similique sit perspiciatis nobis iusto, earum voluptatum perferendis. Fugit harum a dolores! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt ipsa a cupiditate doloribus, aspernatur fuga beatae voluptates deserunt illum unde. Repudiandae, nisi. Ipsam illum saepe alias tempora, nostrum nobis aut? {description}</p>
         <p><strong>Location:</strong> {venue}</p>
         <p><strong>Date:</strong> {date}</p>
         <p><strong>Time:</strong> {time}</p>
-        <button className="buy-ticket-button" onClick={() => window.open(ticketLink, '_blank')}>
+        <div className='session-img'>
+          <img src={'./src/assets/logo-light.png'}/>
+        </div>
+        <button className="session-ticket-button" onClick={() => window.open(ticketLink, '_blank')}>
           Buy Ticket
         </button>
       </div>

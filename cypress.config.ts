8d4d2992ba813cbdb346eca68f8d128f2ccf0c24
Bread <jmagri1@uncc.edu>
2024-12-12 01:48:55 -0500

    directly declared the location of component support file in cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
       viteConfig,
     },
     specPattern: 'cypress/component/*.cy.{js,ts,jsx,tsx}',
+    supportFile: 'cypress/support/component.ts'
   },
   e2e: {
     setupNodeEvents(on, config) {

02ef7d053061c43535178ee4f971b388e0a73fb9
Bread <jmagri1@uncc.edu>
2024-12-12 01:43:05 -0500

    modified support file configuration

--- cypress.config.ts
+++ cypress.config.ts
@@ -10,5 +10,11 @@ export default defineConfig({
       viteConfig,
     },
     specPattern: 'Cypress/component/*.cy.{js,ts,jsx,tsx}',
-  }
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    supportFile: 'Cypress/support/e2e.ts',  // Disable the support file
+  },
 });
